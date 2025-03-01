051128ef84ce70723bf5a663e0a7c1e89e2244bd
mmcarbonara <mmsloth1997@gmail.com>
2025-03-01 11:15:23 +0900

    add

--- src/main/java/com/example/nagoyameshi/service/ReservationService.java
+++ src/main/java/com/example/nagoyameshi/service/ReservationService.java
@@ -9,8 +9,12 @@ public class ReservationService {
 		return numberOfPeople <= capacity;
 	}
 	
+	// 予約時間が営業時間内であるかどうかを確認する
+	public boolean isWithreservedTime(Integer reservedTime, Integer openingTime, Integer closingTime) {
+	    // 予約時間が営業時間内であることを確認できた時のみ予約ができるようにする
+	    return reservedTime >= openingTime && reservedTime <= closingTime;
+	}
 	
-	//予約時間外でないかを確認する
 	
 		
 	}
--- src/main/resources/static/js/flatpickr.js
+++ src/main/resources/static/js/flatpickr.js
@@ -1,9 +1,20 @@
 let maxDate = new Date();
 maxDate = maxDate.setMonth(maxDate.getMonth() + 3);  
 
+//予約日程の設定
 flatpickr('#reservedDate', {
 	mode: "range",
     locale: 'ja',
-    minDate: 'today',
+    minDate: 'today',　//今日の日付のみ選択可能
     maxdate: maxDate
 });
+
+  // 予約時間の設定 
+  flatpickr('#reservedTime', {
+    enableTime: true,        // 時間の選択を有効にする
+    noCalendar: true,        // カレンダーは不要 (時間のみの選択)
+    time_24hr: true,         // 24時間形式で時間を表示
+    dateFormat: "H:i",       // 時間フォーマットを「時:分」に設定
+    minDate: 'today',        // 今日以降の時間のみ選択可能
+    locale: 'ja'          
+  });
