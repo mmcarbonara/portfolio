e8a4fa5f6f85db985fe0b9c09775069adf92e918
mmcarbonara <mmsloth1997@gmail.com>
2025-03-09 10:27:30 +0900

    add

--- src/main/java/com/example/nagoyameshi/controller/CompanyController.java
+++ src/main/java/com/example/nagoyameshi/controller/CompanyController.java
@@ -1,26 +1,31 @@
 package com.example.nagoyameshi.controller;   //トップページに会社情報を表示
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.nagoyameshi.entity.Company;
-import com.example.nagoyameshi.service.CompanyService;
+import com.example.nagoyameshi.repository.CompanyRepository;
 
 @Controller
 @RequestMapping("/company")
 public class CompanyController {
 	
-	@GetMapping("/index")
-	public String companyIndex(Model model) {
-		Optional<Company> company = CompanyService.getCompany();// 会社情報を取得
-		
-	    model.addAttribute("company", company); // モデルに渡す
-		return "company/index";
+	private final CompanyRepository companyRepository;
+	
+	public CompanyController(CompanyRepository companyRepository) {
+		this.companyRepository = companyRepository;
 	}
+	
+	//@GetMapping("index")  //会社概要ページ
+	//public String companyIndex(Model model) {
+	//	Optional<Company> optionalCompany = companyRepository.findById(1); //IDが１の者を取得
+	//	if(optionalCompany.isPresent()) {
+		//	Company company = optionalCompany.get();
+	//	}
+		
+	 //    model.addAttribute("company", company); // モデルに渡す
+	//	return "company/index";
+	
+//}
 }
 
-
+	
